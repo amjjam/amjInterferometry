b5af1e0afd78be8d1b35ad990d3add55d19bd418
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2026-02-17 22:44:45 -0700

    This is a complete rewrite

--- src/amjInterferometry.C
+++ src/amjInterferometry.C
@@ -2,46 +2,56 @@
 
 #include <iostream>
 
-void Phasor::write(amjPacket &p) const{
-  float _real=real();
-  float _imag=imag();
-  p << _real << _imag;
+namespace amjInterferometry{
+  // Begin stolen from Numerical Recipes
+#include <math.h>
+#define SWAP(a,b) tempr=(a);(a)=(b);(b)=tempr
+  
+  void four1(float data[], unsigned long nn, int isign)
+  {
+    unsigned long n,mmax,m,j,istep,i;
+    double wtemp,wr,wpr,wpi,wi,theta;
+    float tempr,tempi;
+    
+    n=nn << 1;
+    j=1;
+    for (i=1;i<n;i+=2) {
+      if (j > i) {
+	SWAP(data[j],data[i]);
+	SWAP(data[j+1],data[i+1]);
+      }
+      m=n >> 1;
+      while (m >= 2 && j > m) {
+	j -= m;
+	m >>= 1;
+      }
+      j += m;
+    }
+    mmax=2;
+    while (n > mmax) {
+      istep=mmax << 1;
+      theta=isign*(6.28318530717959/mmax);
+      wtemp=sin(0.5*theta);
+      wpr = -2.0*wtemp*wtemp;
+      wpi=sin(theta);
+      wr=1.0;
+      wi=0.0;
+      for (m=1;m<mmax;m+=2) {
+	for (i=m;i<=n;i+=istep) {
+	  j=i+mmax;
+	  tempr=wr*data[j]-wi*data[j+1];
+	  tempi=wr*data[j+1]+wi*data[j];
+	  data[j]=data[i]-tempr;
+	  data[j+1]=data[i+1]-tempi;
+	  data[i] += tempr;
+	  data[i+1] += tempi;
+	}
+	wr=(wtemp=wr)*wpr-wi*wpi+wr;
+	wi=wi*wpr+wtemp*wpi+wi;
+      }
+      mmax=istep;
+    }
+  }
+#undef SWAP
+  // End stolen from Numerical Recipes
 }
-
-void Phasor::read(amjPacket &p){
-  float _real;
-  float _imag;
-  p >> _real >> _imag;
-  real(_real);
-  imag(_imag);
-}
-
-void Phasors::write(amjPacket &p) const{
-  p << _name << (int)_p.size();
-  p.reserve(p.size()+_p.size()*sizeof(Phasor));
-  for(unsigned int i=0;i<_p.size();i++)
-    p << _p[i];
-}
-
-void Phasors::read(amjPacket &p){
-  int n;
-  p >> _name >> n;
-  _p.resize(n);
-  for(unsigned int i=0;i<_p.size();i++)
-    p >> _p[i];
-}
-
-void PhasorSets::write(amjPacket &p) const{
-  p << (int)_s.size();
-  for(unsigned int i=0;i<_s.size();i++)
-    p << _s[i];
-}
-
-void PhasorSets::read(amjPacket &p){
-  int n;
-  p >> n;
-  _s.resize(n);
-  for(unsigned int i=0;i<_s.size();i++)
-    p >> _s[i];
-}
-
